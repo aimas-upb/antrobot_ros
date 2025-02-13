235f87d2bd73167125e7670f1931bd202e5e3f66
Dan Novischi <dnovischi@users.noreply.github.com>
2025-02-13 19:51:34 +0200

    Add laser 2 point cloud Update tf laser frame

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         # Install the config folder and its contents
-        ('share/' + package_name + '/config', ['config/antrobot_params.yaml']),
-        ('share/' + package_name + '/config', ['config/keyboard_teleoperation_params.yaml']),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
         # Include all launch files.
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         
